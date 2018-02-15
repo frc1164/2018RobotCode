313c21653d29c16a795d11120bde18809e4327a4
ElijahVela <elijahjvela@gmail.com>
2018-02-14 20:58:34 -0700

    adds CAN VictorSPX library and begins CAN VictorSPX implementation

---
+++ CommandBasedTestBot/src/org/usfirst/frc/team1164/robot/subsystems/Arm.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team1164.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+/**
+ *
+ */
+public class Arm extends Subsystem {
+	
+	VictorSPX Left1 = new VictorSPX(2);
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
